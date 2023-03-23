7680c6ca1a25bde342733385d851fb1ff0e5af1c
konradstarzec <konrad.starzec@zst.kolbuszowa.pl>
2023-03-23 12:12:16 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace  std;
+/*napisz program laczacy dwa slowa*/
+int main() {
+    string imie="Kacper";
+      string nazwisko="Gul";
+      cout<<imie<<" "+ nazwisko;
+
+    return 0;
+}
